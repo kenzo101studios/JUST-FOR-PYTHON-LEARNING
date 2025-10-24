4230fe5af307426d766d599eca5caa2bbc928ecd
Kenzo101 <kenzo101studios@gmail.com>
2025-10-24 17:20:43 -0300

    Update coffee-shop.py

--- coffee-shop.py
+++ coffee-shop.py
@@ -2,6 +2,8 @@
 
 print("Hello, Welcome To The Coffee Shop.")
 
+#BASICS
+
 name = input("What Is Your NAME? \n")
 
 print("Hello " + name + "! Welcome To The Coffee Shop! \n \n \n")
@@ -13,13 +15,17 @@ print("Here Is Our Menu: \n " + menu)
 order = input("\n What Would You Like To Order? \n")
 
 print("Sounds Good " + name + "! We Will Have That " + order +
-      " Ready For You In A Moment.")
+       " Ready For You In A Moment.")
+
+#MORE VARIABLES
 
 price = 5
 
 quantity = input("How Many Would You Like? \n")
 
 total = int(price) * int(quantity)
+
+#TOTAL PRICE
 
 print("Thank You. Your Total Is: \n")
 print("$" + str(total))
