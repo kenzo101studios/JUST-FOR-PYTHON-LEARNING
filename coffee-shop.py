48d352e79e31f93fb5ba5bd7a9ef9d861ee20815
Kenzo101 <kenzo101studios@gmail.com>
2025-10-24 20:13:45 +0000

    Add coffee-shop.py and change .replit in Version 1.5

---
+++ coffee-shop.py
@@ -0,0 +1,25 @@
+#INPUT FOR SCRIPT
+
+print("Hello, Welcome To The Coffee Shop.")
+
+name = input("What Is Your NAME? \n")
+
+print("Hello " + name + "! Welcome To The Coffee Shop! \n \n \n")
+
+menu = "Espresso, Latte, Cappuccino, Frappuccino, Tea, Coffee, Hot Chocolate, Water"
+
+print("Here Is Our Menu: \n " + menu)
+
+order = input("\n What Would You Like To Order? \n")
+
+print("Sounds Good " + name + "! We Will Have That " + order +
+      " Ready For You In A Moment.")
+
+price = 5
+
+quantity = input("How Many Would You Like? \n")
+
+total = int(price) * int(quantity)
+
+print("Thank You. Your Total Is: \n")
+print("$" + str(total))
